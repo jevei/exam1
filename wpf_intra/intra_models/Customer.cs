86320b0f2650f505fc94e11a4d02e0b5c8ea3cf3
jevei <j.veillette@hotmail.ca>
2020-02-24 13:15:44 -0500

    1

---
+++ wpf_intra/intra_models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace intra_models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactInfo { get; set; }
+        public string PicturePath { get; set; }
+        public string Info { get; set; }
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
